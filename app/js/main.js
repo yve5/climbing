e97354fcb22ef2c62196cbe6a398c5b8daf28b6e
Yve5 <yves.rouille@gmail.com>
2017-10-24 21:03:25 +0200

    routing work

--- app/js/main.js
+++ app/js/main.js
@@ -1,11 +1,50 @@
 function ClimbingViewModel() {
     var self = this;
+ 
+    self.root = ko.observable('');
+    self.hash = ko.observable('#!');
+ 
+    self.init = function() {
+        self.listen();
+    }
+ 
+    self.listen = function() {
+        if (self.root() !== self.getFragment()) {
+            self.root(self.getFragment());
+        }
+ 
+        requestAnimationFrame(self.listen);
+    }
+ 
+    self.getFragment = function() {
+        var fragment = '';
+        var regEx    = new RegExp(self.hash() + '(.*)$');
+        var match    = window.location.href.match(regEx);
+ 
+        fragment = match ? match[1] : '';
+ 
+        return self.clearSlashes(fragment);
+    }
+ 
+    self.clearSlashes = function(path) {
+        return path.toString().replace(/\/$/, '').replace(/^\//, '');
+    }
+ 
+    // self.navigate = function(path) {
+    //     path = path ? path : '';
+    //     window.location.href = window.location.href.replace(/#(.*)$/, '') + '#' + path;
+    // }
+ 
+    self.checkPage = function(page) {
+        if (page === 'home') {
+            return self.root() === '';
+        }
+        else {
+            return self.root() === page;
+        }
+    }
 }
-
+ 
 var cvm = new ClimbingViewModel();
-
-pager.Href.hash = '#!/';
-
-pager.extendWithPage(cvm);
 ko.applyBindings(cvm);
-pager.start();
+cvm.init();
