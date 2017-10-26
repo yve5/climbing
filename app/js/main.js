f2ad56a2d33ef01f050612ab6abafb6bc326ba08
Yve5 <yves.rouille@gmail.com>
2017-10-27 01:50:44 +0200

    navigate

--- app/js/main.js
+++ app/js/main.js
@@ -1,40 +1,62 @@
 function ClimbingViewModel() {
     var self = this;
- 
-    self.root = ko.observable('');
-    self.hash = ko.observable('#!');
- 
+
+    self.root   = ko.observable('');
+    self.hash   = ko.observable('#!');
+    self.routes = ['excursion', 'events', 'photos'];
+
     self.init = function() {
         self.listen();
     }
- 
+
     self.listen = function() {
         if (self.root() !== self.getFragment()) {
+            if (self.getFragment() !== '' && self.routes.indexOf(self.getFragment()) === -1) {
+                self.navigate('');
+            }
+
             self.root(self.getFragment());
         }
- 
+
         requestAnimationFrame(self.listen);
     }
- 
-    self.getFragment = function() {
-        var fragment = '';
-        var regEx    = new RegExp(self.hash() + '(.*)$');
-        var match    = window.location.href.match(regEx);
- 
-        fragment = match ? match[1] : '';
- 
-        return self.clearSlashes(fragment);
+
+    self.getFragment = function(index) {
+        var fragments, regEx, match;
+
+        if (index === undefined) {
+            index = 0;
+        }
+
+        fragments = '';
+        regEx     = new RegExp(self.hash() + '(.*)$');
+        match     = window.location.href.match(regEx);
+
+        fragments = match ? match[1] : '';
+        fragments = self.clearSlashes(fragments);
+        fragments = fragments.split('/');
+
+        if (index >= fragments.length) {
+            index = fragments.length - 1;
+        }
+
+        return fragments[index];
     }
- 
+
     self.clearSlashes = function(path) {
         return path.toString().replace(/\/$/, '').replace(/^\//, '');
     }
- 
-    // self.navigate = function(path) {
-    //     path = path ? path : '';
-    //     window.location.href = window.location.href.replace(/#(.*)$/, '') + '#' + path;
-    // }
- 
+
+    self.navigate = function(path) {
+        if (path === '') {
+            window.location.href = window.location.href.replace(/#(.*)$/, '') + '#';
+        }
+        else {
+            path = path ? path : '';
+            window.location.href = window.location.href.replace(/#(.*)$/, '') + self.hash() + '/' + path;
+        }
+    }
+
     self.checkPage = function(page) {
         if (page === 'home') {
             return self.root() === '';
@@ -44,7 +66,7 @@ function ClimbingViewModel() {
         }
     }
 }
- 
+
 var cvm = new ClimbingViewModel();
 ko.applyBindings(cvm);
 cvm.init();
