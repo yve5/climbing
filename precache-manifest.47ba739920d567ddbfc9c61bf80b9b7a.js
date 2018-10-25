92514edb1fa6f85bf01b19fcc033e657a8591b1f
Yves Rouillé <yrouille@carboatmedia.fr>
2018-10-25 18:22:17 +0200

    initial commit for create-react-app build

---
+++ precache-manifest.47ba739920d567ddbfc9c61bf80b9b7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2fc2eee541496e9c5f9b",
+    "url": "/static/js/main.2fc2eee5.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "2fc2eee541496e9c5f9b",
+    "url": "/static/css/main.bf9de5a2.chunk.css"
+  },
+  {
+    "revision": "110319c4fcf7a2fe30cbb51e55cec6c8",
+    "url": "/index.html"
+  }
+];
