5138af70d4c7d0d6b208c404d17bb67657cfa777
Yves <yves.rouille@gmail.com>
2018-11-06 01:40:29 +0100

    router

--- src/App.js
+++ src/App.js
@@ -13,6 +13,12 @@ import {
   DropdownItem
 } from 'reactstrap';
 
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+const Index = () => <h2>Home</h2>;
+const About = () => <h2>About</h2>;
+const Users = () => <h2>Users</h2>;
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -31,61 +37,73 @@ class App extends Component {
 
   render() {
     return (
-      <Container className="App">
-        <Row>
-          <Col>
-            <img src={logo} className="App-logo" alt="logo" />
+      <Router>
+        <Container className="App">
+          <Row>
+            <Col>
+              <Link to="/">Home</Link>
+              <Link to="/about">About</Link>
+              <Link to="/users">Users</Link>
 
-            <p>
-              Edit <code>src/App.js</code> and save to reload.
-            </p>
+              <Route path="/" exact component={Index} />
+              <Route path="/about/" component={About} />
+              <Route path="/users/" component={Users} />
+              
+              
 
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <h1>Button</h1>
-            
-            <Row>
-              <Col>
-                <Button color="danger">Hello World!</Button>
-              </Col>
-              <Col className="text-right">
-                <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
-                  <DropdownToggle caret>
-                    Dropdown
-                  </DropdownToggle>
-                  <DropdownMenu>
-                    <DropdownItem header>Header</DropdownItem>
-                    <DropdownItem disabled>Action</DropdownItem>
-                    <DropdownItem>Another Action</DropdownItem>
-                    <DropdownItem divider />
-                    <DropdownItem>Another Action</DropdownItem>
-                  </DropdownMenu>
-                </Dropdown>
-              </Col>
-            </Row>
+              <img src={logo} className="App-logo" alt="logo" />
 
-            <p className="text-justify">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-              quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-              consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-              cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-              proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
+              <p>
+                Edit <code>src/App.js</code> and save to reload.
+              </p>
 
-          </Col>
-        </Row>
-      </Container>
+              <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Learn React
+              </a>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <h1>Button</h1>
+              
+              <Row>
+                <Col>
+                  <Button color="danger">Hello World!</Button>
+                </Col>
+                <Col className="text-right">
+                  <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
+                    <DropdownToggle caret>
+                      Dropdown
+                    </DropdownToggle>
+                    <DropdownMenu>
+                      <DropdownItem header>Header</DropdownItem>
+                      <DropdownItem disabled>Action</DropdownItem>
+                      <DropdownItem>Another Action</DropdownItem>
+                      <DropdownItem divider />
+                      <DropdownItem>Another Action</DropdownItem>
+                    </DropdownMenu>
+                  </Dropdown>
+                </Col>
+              </Row>
+
+              <p className="text-justify">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+
+            </Col>
+          </Row>
+        </Container>
+      </Router>
     );
   }
 }
