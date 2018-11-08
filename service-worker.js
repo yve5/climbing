6b7e22ffdd44793ed7cccf2d7d28cf3557a6d84e
Yves Rouillé <yrouille@carboatmedia.fr>
2018-11-08 14:34:06 +0100

    router

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/climbing/precache-manifest.73b02d483467dd91b6aae5168ad4659f.js"
+  "/climbing/precache-manifest.02affb0370860009ce35dcccc1d2aa75.js"
 );
 
 workbox.clientsClaim();
