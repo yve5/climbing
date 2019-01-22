70e37429e952e7b9aeb867c14640ebe4af917ef2
Yves <yves.rouille@gmail.com>
2019-01-23 00:38:52 +0100

    lorem

--- src/Features/Accueil/Accueil.js
+++ src/Features/Accueil/Accueil.js
@@ -16,7 +16,24 @@ import {
  } from 'reactstrap';
 
 class Accueil extends Component {
+  constructor(props) {
+    super(props);
+
+    this.getLoremIpsum = this.getLoremIpsum.bind(this);
+  }
+
+  getLoremIpsum(key) {
+    return (
+      <p key={key}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+    )
+  }
+  
   render() {
+    let collection = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    let fakeContent = collection.map(data => this.getLoremIpsum(data));
+
     return (
       <div>
         <Row>
@@ -49,25 +66,11 @@ class Accueil extends Component {
           </Col>
         </Row>
 
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.
-        </p>
+        {fakeContent}
 
-        <p>
-          Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. 
-        </p>
+        <img src={lunettes} alt="lunettes" />
 
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </p>
-
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </p>
-
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        </p>
+        {fakeContent}
       </div>
     );
   }
